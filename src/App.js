5bc7f2177847f325b7ae061186455a6aaf1610fc
madhavi <madhavireddy6833@gmail.com>
2024-12-29 18:34:30 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,55 @@
 import React from "react";
 import "./index.css";
 
-const Table = () => {
-  const data = [
-    {
-      name: "xyz",
-      details: "For one of the client",
-      contact: "+91-234567895",
-      email: "xyz@gmail.com",
-      paymentMethod: "Online/Card",
-    },
-  
-    {
-      name: "xyz1",
-      details: "For one of the client2",
-      contact: "+91-4567891235",
-      email: "xyz123@gmail.com",
-      paymentMethod: "Online/Card",
-    },
-    {
-      name: "chandu",
-      details: "For one of the client2",
-      contact: "+91-4567891678",
-      email: "xyz123@gmail.com",
-      paymentMethod: "Online/Card",
-    },
-  ];
-
+function LoginForm() {
   return (
-    <div className="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Organization name</th>
-            <th>Organization details</th>
-            <th>Organization contact number</th>
-            <th>Organization mail id</th>
-            <th>Organization payment method</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((item, index) => (
-            <tr key={index}>
-              <td>{item.name}</td>
-              <td>{item.details}</td>
-              <td>{item.contact}</td>
-              <td>{item.email}</td>
-              <td>{item.paymentMethod}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="login-container">
+      <div className="login-box">
+        <div className="avatar">
+          <img
+            src="https://via.placeholder.com/60"
+            alt="Avatar"
+            className="avatar-img"
+          />
+        </div>
+        <h2>LOGIN</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="username">
+              <i className="fa fa-user"></i>
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Username"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">
+              <i className="fa fa-lock"></i>
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="checkbox-group">
+            <input type="checkbox" id="remember" name="remember" />
+            <label htmlFor="remember">Remember me</label>
+          </div>
+          <button type="submit" className="login-btn">
+            LOGIN
+          </button>
+        </form>
+        <p className="forgot-link">
+          <a href="/">Forgot Username / Password?</a>
+        </p>
+      </div>
     </div>
   );
-};
+}
 
-export default Table
+export default LoginForm;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import('tailwindcss').Config
+module.exports = {
+  theme: {
+    screens: {
+      sm: '640px',  // Small devices (phones)
+      md: '768px',  // Medium devices (tablets)
+      lg: '1024px', // Large devices (desktops)
+      xl: '1280px', // Extra large devices
+      '2xl': '1536px', // 2X large devices
+    },
+  },
+};
+
+
